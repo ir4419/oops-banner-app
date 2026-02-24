031abdc7189e3354b670f195f4ca2a0296d570f9
Ishita Rayudu <ir4419@srmist.edu.in>
2026-02-24 13:12:34 +0530

    Feature(Uc2) : Render OOPS as Banner using Print Statements

--- src/com.oopsbanner/OOpsBannerApp.java
+++ src/com.oopsbanner/OOpsBannerApp.java
@@ -1,5 +1,12 @@
 public class OOpsBannerApp{
     public static void main(String[] args) {
 	    System.out.println("OOPs") ;
+		System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *    ");
+        System.out.println("*     *  *     *  *     *  *    ");
+        System.out.println("*     *  *     *  *****   ***** ");
+        System.out.println("*     *  *     *  *            *");
+        System.out.println("*     *  *     *  *            *");
+        System.out.println(" *****   *****   *        ***** ");
 	}
 }	
