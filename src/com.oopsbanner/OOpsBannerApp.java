bb94133c3cbf25a995eba1ff384d7a9540ef412d
Ishita Rayudu <ir4419@srmist.edu.in>
2026-02-24 15:52:09 +0530

    feature(uc6): staticfunction

--- src/com.oopsbanner/OOpsBannerApp.java
+++ src/com.oopsbanner/OOpsBannerApp.java
@@ -1,16 +1,51 @@
 public class OOpsBannerApp{
     public static void main(String[] args) {
-		   String[] banner = {
-                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*     *", "*      "),
-                String.join("   ", "*     *", "*     *", "*     *", "*      "),
-                String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*      ", "      *"),
-                String.join("   ", "*     *", "*     *", "*      ", "      *"),
-                String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+ return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
         };
+    }
 
-        for (String line : banner) {
+    // Method to generate letter P
+    public static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Method to generate letter S
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O1 = getO();
+        String[] O2 = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        for (int i = 0; i < 7; i++) {
+            String line = String.join("   ", O1[i], O2[i], P[i], S[i]);
             System.out.println(line);
 		}
        
