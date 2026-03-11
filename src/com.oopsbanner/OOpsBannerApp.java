36cf1108942d4df2b74674f125f6ed7754cdc2bd
Ishita Rayudu <ir4419@srmist.edu.in>
2026-03-11 13:04:59 +0530

    feature(uc8): UseMapforCharacterPatternsandRenderviaFunction

--- src/com.oopsbanner/OOpsBannerApp.java
+++ src/com.oopsbanner/OOpsBannerApp.java
@@ -1,29 +1,11 @@
-public class OOpsBannerApp{
-    public static void main(String[] args) {
- static class CharacterPatternMap {
-        private char character;
-        private String[] pattern;
-
-        public CharacterPatternMap(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-
-        public char getCharacter() {
-            return character;
-        }
-
-        public String[] getPattern() {
-            return pattern;
-        }
-    }
+public class OOpsBannerApp {
 
     public static void main(String[] args) {
 
-        // Create map to store character patterns
-        Map<Character, CharacterPatternMap> patternMap = new HashMap<>();
+        Map<Character, String[]> patternMap = new HashMap<>();
 
-        patternMap.put('O', new CharacterPatternMap('O', new String[]{
+     
+        patternMap.put('O', new String[]{
                 " ***** ",
                 "*     *",
                 "*     *",
@@ -31,9 +13,10 @@ public class OOpsBannerApp{
                 "*     *",
                 "*     *",
                 " ***** "
-        }));
+        });
 
-        patternMap.put('P', new CharacterPatternMap('P', new String[]{
+     
+        patternMap.put('P', new String[]{
                 " ***** ",
                 "*     *",
                 "*     *",
@@ -41,9 +24,10 @@ public class OOpsBannerApp{
                 "*      ",
                 "*      ",
                 "*      "
-        }));
+        });
 
-        patternMap.put('S', new CharacterPatternMap('S', new String[]{
+      
+        patternMap.put('S', new String[]{
                 " ***** ",
                 "*      ",
                 "*      ",
@@ -51,21 +35,24 @@ public class OOpsBannerApp{
                 "      *",
                 "      *",
                 " ***** "
-        }));
+        });
 
         String word = "OOPS";
 
+       
         for (int i = 0; i < 7; i++) {
             StringBuilder line = new StringBuilder();
 
             for (char ch : word.toCharArray()) {
-                String[] pattern = patternMap.get(ch).getPattern();
+                String[] pattern = patternMap.get(ch);
                 line.append(pattern[i]).append("   ");
             }
 
             System.out.println(line);
-		}
-       
+        }
     }
 }
+
+
+
 	
